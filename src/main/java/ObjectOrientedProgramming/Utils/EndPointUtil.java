1d22093645cbebce0ae59dd361ac6b10b4ca43a2
fatihtopcuoglu <vmax78@hotmail.com>
2021-01-08 00:31:45 +0100

    initial setup UnitTest v1.o

--- src/main/java/ObjectOrientedProgramming/Utils/EndPointUtil.java
+++ src/main/java/ObjectOrientedProgramming/Utils/EndPointUtil.java
@@ -3,20 +3,26 @@ package ObjectOrientedProgramming.Utils;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.parsing.Parser;
-import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
+
 
 public class EndPointUtil {
 
 
-        public static Response doGetRequest(String endpoint) {
-            RestAssured.defaultParser = Parser.JSON;
-            return
-            given().headers("Content-Type", ContentType.JSON, "Accept", ContentType.JSON).
-                    when().get(endpoint).
-                    then().contentType(ContentType.JSON).extract().response();
+    public static void doGetRequest(String endpoint) {
+        RestAssured.defaultParser = Parser.JSON;
+        given().headers("Content-Type", ContentType.JSON, "Accept", ContentType.JSON).
+                when().
+                get(endpoint).
+                then().
+                assertThat().
+                statusCode(200).
+                body(matchesJsonSchemaInClasspath("result.json")).
+                body("season", contains("2017"));
 
     }
-
 }
--- src/test/java/UnitTestEndPoint.java
+++ src/test/java/UnitTestEndPoint.java
@@ -9,10 +9,7 @@ import static ObjectOrientedProgramming.Utils.EndPointUtil.doGetRequest;
 
 public class UnitTestEndPoint extends EndPointUtil {
     @Test
-    public void testGetPointPen() {
-        Response response = doGetRequest("https://jsonplaceholder.typicode.com/users/");
-        Map<String, String> company = response.jsonPath().getMap("company[0]");
-        System.out.println(company);
-
+    public void testGetEndPoint() {
+        doGetRequest("http://ergast.com/api/f1/2017/circuits.json");
     }
 }

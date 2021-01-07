ffcf40eb012b51efeed0e8c172db222a4a267b2b
fatihtopcuoglu <vmax78@hotmail.com>
2021-01-07 19:42:05 +0100

    initial setup UnitTest v0.8

--- src/test/java/UnitTestEndPoint.java
+++ src/test/java/UnitTestEndPoint.java
@@ -1,10 +1,18 @@
+import ObjectOrientedProgramming.Utils.EndPointUtil;
+import io.restassured.response.Response;
 import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
 
 import static ObjectOrientedProgramming.Utils.EndPointUtil.doGetRequest;
 
-public class UnitTestEndPoint {
+public class UnitTestEndPoint extends EndPointUtil {
     @Test
     public void testGetPointPen() {
-        doGetRequest("http://ergast.com/api/f1/2017/circuits.json");
+        Response response = doGetRequest("https://jsonplaceholder.typicode.com/users/");
+        Map<String, String> company = response.jsonPath().getMap("company[0]");
+        System.out.println(company);
+
     }
 }

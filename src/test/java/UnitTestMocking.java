0065ae586b0ef29fe95aae185759813389a26a36
fatih78 <vmax78@hotmail.com>
2021-01-14 09:24:44 +0100

    Mocking examples: v2.7

--- src/test/java/UnitTestMocking.java
+++ src/test/java/UnitTestMocking.java
@@ -10,6 +10,14 @@ import org.junit.Test;
 
 public class UnitTestMocking {
 
+    /*
+    Mocking is primarily used in unit testing. An object under test may have dependencies on other (complex) objects.
+    To isolate the behavior of the object you want to replace the other objects by mocks that simulate the behavior of the real objects.
+    This is useful if the real objects are impractical to incorporate into the unit test.
+
+     In short, mocking is creating objects that simulate the behavior of real objects.
+     */
+
     @Test
     public void testMocking() {
         // you mock data in your test

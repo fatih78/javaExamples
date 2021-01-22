991cecbe3e9b089c94c6bcc50cc22881c638095d
fatihtopcuoglu <vmax78@hotmail.com>
2021-01-22 22:36:48 +0100

    updated Sulo class

---
+++ src/test/java/UnitTestSulo.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class UnitTestSulo extends Sulo {
+
+    Sulo s = new Sulo();
+
+    @Test
+    public  void UnitTestSulo1(){
+        s.ifloop1(123, 123, 100);
+        System.out.println(result);
+        Assert.assertEquals(result, "A is niet gelijk aan B");
+    }
+}

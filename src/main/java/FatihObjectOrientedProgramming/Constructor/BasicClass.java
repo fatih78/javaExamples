6c74349cc9e7ca5bed38d64b2e97ed4cac9b8902
fatihtopcuoglu <vmax78@hotmail.com>
2021-02-06 00:17:02 +0100

    New Constructor cases added v3

--- src/main/java/FatihObjectOrientedProgramming/Constructor/BasicClass.java
+++ src/main/java/FatihObjectOrientedProgramming/Constructor/BasicClass.java
@@ -5,10 +5,16 @@ public class BasicClass {
     public int x;
     public int modelYear;
     public String modelName;
+    public int result;
 
     public BasicClass(int x, int modelYear, String modelName) {
         this.x = x;
         this.modelYear = modelYear;
         this.modelName = modelName;
+        result = calculate();
+    }
+
+    public int calculate (){
+        return x * 2;
     }
 }

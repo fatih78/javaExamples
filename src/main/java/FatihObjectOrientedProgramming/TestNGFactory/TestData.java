2c71755956ed79a1aa06629742b21e0f0acee740
fatihtopcuoglu <vmax78@hotmail.com>
2021-02-07 20:04:40 +0100

    TestNG Factory version1

---
+++ src/main/java/FatihObjectOrientedProgramming/TestNGFactory/TestData.java
@@ -0,0 +1,11 @@
+package FatihObjectOrientedProgramming.TestNGFactory;
+
+public class TestData {
+        public static String testdata;
+        public static int objectID;
+
+        public TestData(int objectID, String testdata) {
+            TestData.objectID = objectID;
+            TestData.testdata = testdata;
+        }
+}
---
+++ src/test/java/TestNGFactoryExample.java
@@ -0,0 +1,33 @@
+import FatihObjectOrientedProgramming.TestNGFactory.TestData;
+import org.junit.Assert;
+import org.testng.annotations.Factory;
+import org.testng.annotations.Test;
+
+public class TestNGFactoryExample extends TestData {
+
+    public TestNGFactoryExample(int objectID, String testdata) {
+        super(objectID, testdata);
+    }
+
+    @Test
+    public void testmethod() {
+        System.out.println(objectID + ":" + "" + "This is test method, testdata is  :" + testdata);
+    }
+
+    public static class TestNGFactory {
+        @Factory
+        public static Object[] testData() {
+            Object[] object = new Object[3];
+
+            object[0] = new TestNGFactoryExample(1, "testdata1");
+            Assert.assertEquals(testdata, "testdata1");
+            object[1] = new TestNGFactoryExample(2, "testdata2");
+            Assert.assertEquals(testdata, "testdata2");
+            object[2] = new TestNGFactoryExample(3, "testdata3");
+            Assert.assertEquals(testdata, "testdata3");
+
+            return object;
+        }
+    }
+
+}

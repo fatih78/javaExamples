09a9650fb4f19b93fcb6be08a8c9f22bb3aad518
fatihtopcuoglu <vmax78@hotmail.com>
2021-02-09 22:45:43 +0100

    Lmabda Expression version4

--- src/main/java/FatihObjectOrientedProgramming/LambdaExpressions/Greetings.java
+++ src/main/java/FatihObjectOrientedProgramming/LambdaExpressions/Greetings.java
@@ -1,8 +1,21 @@
 package FatihObjectOrientedProgramming.LambdaExpressions;
 
 public class Greetings {
+    String result;
 
     public interface MyGreeting {
         String processName(String str);
+    }
+
+    public String morningGreeting() {
+        MyGreeting morningGreeting = (str) -> "Good Morning " + str + "!";
+        System.out.println(morningGreeting.processName("Fatih"));
+        return result = morningGreeting.processName("Fatih");
+    }
+
+    public String  eveningGreeting(){
+        MyGreeting eveningGreeting = (str) -> "Good Evening " + str + "!";
+        System.out.println(eveningGreeting.processName("Fatih"));
+        return result = eveningGreeting.processName("Fatih");
     }
 }
--- src/test/java/UnitTestLambdaExpressions.java
+++ src/test/java/UnitTestLambdaExpressions.java
@@ -6,28 +6,21 @@ import org.junit.Test;
 public class UnitTestLambdaExpressions {
     Numbers n = new Numbers();
     Greetings g = new Greetings();
-    String result;
 
 
     @Test
-    public void testNumbers(){
-        n.numbers().forEach((n) -> {System.out.println(n);});
+    public void testNumbers() {
+        n.numbers().forEach((n) -> { System.out.println(n);});
         Assert.assertTrue(n.numbers.size() == 4);
     }
 
     @Test
     public void greetings() {
-        Greetings.MyGreeting morningGreeting = (str) -> "Good Morning " + str + "!";
-        System.out.println(morningGreeting.processName("Fatih"));
-        result = morningGreeting.processName("Fatih");
-        Assert.assertEquals(result, "Good Morning Fatih!");
+        Assert.assertEquals(g.morningGreeting(), "Good Morning Fatih!");
     }
 
     @Test
     public void greetings2() {
-        Greetings.MyGreeting morningGreeting = (str) -> "Good Evening " + str + "!";
-        System.out.println(morningGreeting.processName("Fatih"));
-        result = morningGreeting.processName("Fatih");
-        Assert.assertEquals(result, "Good Evening Fatih!");
+        Assert.assertEquals(g.eveningGreeting(), "Good Evening Fatih!");
     }
 }

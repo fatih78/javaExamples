163d27b19b1abe3a3bea46b2bd096f72e94f4d1e
fatih78 <vmax78@hotmail.com>
2021-03-09 09:22:06 +0100

    Primitives: paramaters & constants - v7

--- src/test/java/UnitTestPrimitives.java
+++ src/test/java/UnitTestPrimitives.java
@@ -4,6 +4,9 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import static FatihObjectOrientedProgramming.Utils.CalculatorSumUtil.result2;
+import static net.bytebuddy.matcher.ElementMatchers.is;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.Assert.assertThat;
 
 public class UnitTestPrimitives extends RandomNumbers {
     CalculatorController s = new CalculatorController();
@@ -11,13 +14,14 @@ public class UnitTestPrimitives extends RandomNumbers {
     @Test
     public void primitivesTest1() {
         s.CalculatorSum(TRANSACTION_ID, 2);
+        assertThat(TRANSACTION_ID, instanceOf(Integer.class));
         System.out.println(TRANSACTION_ID);
     }
 
     @Test
     public void primitivesTest2() {
         s.CalculatorSumDouble(DOUBLE_ID, 2.2);
-//        Assert.assertEquals(result2,);
+        assertThat(DOUBLE_ID, instanceOf(Double.class));
         System.out.println(DOUBLE_ID);
     }
 
